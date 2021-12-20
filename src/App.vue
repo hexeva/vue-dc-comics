f9cb892ec123513166ae85d4ab50919e9ddab089
hexeva <patrick79.g@gmail.com>
2021-12-20 17:04:43 +0100

    push

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <Header />
+
+      <main>Qui ci sarà il main</main>
+    </div>
+    
+    
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+
+import Header from "./components/Header.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Header,
+    
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './style/general.scss';
+// *{
+//   margin: 0;
+//   padding: 0;
+//   box-sizing: border-box;
+// }
+// img{
+//   width: 100%;
+// }
+// .container{
+//   width: 70%;
+//   margin: auto;
+//   height:100%;
+//   /* test */
+//   background-color: antiquewhite;
+// }
+  
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,70 @@
+<template>
+    <header>
+        <div class="menu-container">
+            <div class="left-col">
+                <div class="logo">
+                    <img src="../assets/img/dc-logo.png" alt="logo dc">
+
+                </div>
+            </div>
+            <div class="right-col">
+                <ul>
+                    <li>CHARACTERS</li>
+                    <li>CHARACTERS</li>
+                    <li>CHARACTERS</li>
+                    <li>CHARACTERS</li>
+                    <li>CHARACTERS</li>
+                    <li>CHARACTERS</li>
+                    <li>CHARACTERS</li>
+                    <li>CHARACTERS</li>
+                    <li>CHARACTERS</li>
+                    <li>CHARACTERS</li>
+
+                </ul>
+            </div>
+
+        </div>
+    </header>
+</template>
+
+<script>
+    export default{
+        name:'Header',
+
+}
+</script>
+
+
+<style scoped>
+    header{
+        width: 100%;
+        height: 120px;
+        line-height: 120px;
+        /* test */
+        background-color: aquamarine;
+    }
+    .left-col{
+        height: 100%;
+        width: 50%;
+        float: left;
+        
+        
+    }
+    .right-col{
+        width: 50%;
+        height: 100%;
+        float: left; 
+    }
+
+    .right-col ul li{
+        display: inline-block;
+    }
+    .logo{
+        width: 85px;
+        display: inline-block;
+    }
+    .logo img{
+        vertical-align: middle;
+    }
+    
+</style>
