545509f7591eb031d7c62e1dca14d44bd1139581
hexeva <patrick79.g@gmail.com>
2021-12-21 17:03:25 +0100

    added footer links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,118 @@
 <template>
     <footer>
-        io sono il footer
+        <div class="footer-top">
+            <div class="container">
+                  <!-- links section -->
+                    <div class="links-section">
+                        <div class="links">
+                            <h2>dc comics</h2>
+                            <ul>
+                                <li>
+                                    <a href="#">Characters</a>
+                                </li>
+                                <li>
+                                    <a href="#">Comics</a>
+                                </li>
+                                <li>
+                                    <a href="#">Movies</a>
+                                </li>
+                                <li>
+                                    <a href="#">TV</a>
+                                </li>
+                                <li>
+                                    <a href="#">Games</a>
+                                </li>
+                                <li>
+                                    <a href="#">Videos</a>
+                                </li>
+                                <li>
+                                    <a href="#">News</a>
+                                </li>
+                            </ul>
+                            <h2>shop</h2>
+                            <ul>
+                                <li>
+                                    <a href="#">Shop DC</a>
+                                </li>
+                                <li>
+                                    <a href="#">Shop DC Collectibles</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- end links -->
+                        <div class="links">
+                            <h2>dc</h2>
+                            <ul>
+                                <li>
+                                    <a href="#">Terms of Use</a>
+                                </li>
+                                <li>
+                                    <a href="#">Privacy policy (New)</a>
+                                </li>
+                                <li>
+                                    <a href="#">Ad choices</a>
+                                </li>
+                                <li>
+                                    <a href="#">Advertising</a>
+                                </li>
+                                <li>
+                                    <a href="#">Jobs</a>
+                                </li>
+                                <li>
+                                    <a href="#">Subscriptions</a>
+                                </li>
+                                <li>
+                                    <a href="#">Talent Workshops</a>
+                                </li>
+                                 <li>
+                                    <a href="#">CPSC Certificated</a>
+                                </li>
+                                <li>
+                                    <a href="#">Rating</a>
+                                </li>
+                                <li>
+                                    <a href="#">Shop Help</a>
+                                </li>
+                                <li>
+                                    <a href="#">Contact Us</a>
+                                </li>
+                            </ul>
+                            
+                        </div>
+                        <!-- end links -->
+                        <div class="links">
+                            <h2>sites</h2>
+                            <ul>
+                                <li>
+                                    <a href="#">DC</a>
+                                </li>
+                                <li>
+                                    <a href="#">MAD Magazine</a>
+                                </li>
+                                <li>
+                                    <a href="#">DC Kids</a>
+                                </li>
+                                <li>
+                                    <a href="#">Dc Universe</a>
+                                </li>
+                                <li>
+                                    <a href="#">DC Power Visa</a>
+                                </li>
+                                
+                            </ul>
+                           
+                        </div>
+                        <!-- end links -->
+                    </div>
+
+                    <!-- end links sections -->
+                    <div class="dc_logo"></div>
+                </div>
+                <!-- end container -->
+                
+        </div>
+          
+          
     </footer>
 </template>
 
@@ -21,11 +133,36 @@
 
 footer {
     width: 100%;
-    background-image: url(../assets/img/footer-bg.jpg);
-    @include background_general;
+
+        .footer-top{
+            background-image: url(../assets/img/footer-bg.jpg);
+            @include background_general;
+           
+
+            .links-section{
+                width: 50%;
+                padding: 35px 0;
+                display: flex;
+                .links{
+                    margin-right:30px;
+
+                    li{
+                    margin: 8px 0;
+                    a{
+                        color:$brand_footer_color;
+                    }
+                }
+
+                }
+                
+            }
+        }
+
+        
     
-    // test
-    height: 400px;
+}
+h2{
+    margin:20px 0;
 }
 
 </style>
--- src/components/SingleProduct.vue
+++ src/components/SingleProduct.vue
@@ -35,6 +35,7 @@ export default {
     .product-title{
         text-transform: uppercase;
         
+        
 
     }
 
